8c469f2d9a6e69b09be0d6e7efd86edf25eb6b0c
Umut Akkaya <umutakkaya1996@gmail.com>
2018-08-28 00:51:00 +0300

    first commit

---
+++ src/Providers/OrbitaliServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Orbitali\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class OrbitaliServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
