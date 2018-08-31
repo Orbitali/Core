6abf25e587e2d33642c32bd0880f70150c9db8dc
Umut Akkaya <umutakkaya1996@gmail.com>
2018-08-31 03:09:32 +0300

    Fixing for Cache

--- src/Http/Middleware/CacheRequest.php
+++ src/Http/Middleware/CacheRequest.php
@@ -2,6 +2,8 @@
 
 namespace Orbitali\Http\Middleware;
 
+use Orbitali\Foundations\ResponseSerializer;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Session;
@@ -16,15 +18,49 @@ class CacheRequest
      */
     public function handle($request, $next)
     {
+        /**
+         * @var Response $a
+         */
+        if ($this->shouldCacheRequest($request)) {
+            $key = $this->getCacheKey($request);
+            if (Cache::has($key)) {
+                $response = Cache::get($key);
+                return (new ResponseSerializer())->unserialize($response);
+            } else {
+                $response = $next($request);
+                if ($this->shouldCacheResponse($response)) {
+                    Cache::put($key, (new ResponseSerializer())->serialize($response), 1440);
+                }
+                return $response;
+            }
+        } else {
+            return $next($request);
+        }
+    }
+
+    private function getCacheKey($request)
+    {
         $arrayExceptingItems = ["_previous", "PHPDEBUGBAR_STACK_DATA"];
         if (!Auth::check()) {
             $arrayExceptingItems[] = "_token";
         }
-        return Cache::get(
-            hash("md4", $request->fullUrl() . "#" . serialize(array_except(Session::all(), $arrayExceptingItems))),
-            function () use ($next, $request) {
-                return $next($request);
-            }
-        );
+        return "orbitali.cache.middleware." . mb_strtolower($request->getMethod()) . "." . hash("md4", $request->fullUrl() . "#" . serialize(array_except(Session::all(), $arrayExceptingItems)));
+    }
+
+    private function shouldCacheRequest($request): bool
+    {
+        return !$request->ajax() && $request->isMethod('get');
+        /*if ($request->ajax()) {
+            return false;
+        }
+        if ($this->isRunningInConsole()) {
+            return false;
+        }
+        return $request->isMethod('get');*/
+    }
+
+    private function shouldCacheResponse($response): bool
+    {
+        return $response->isSuccessful() || $response->isRedirection();
     }
 }
---
+++ src/Http/Traits/Cacheable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orbitali\Http\Traits;
+
+
+trait Cacheable
+{
+    /**
+     * Calculate a unique cache key for the model instance.
+     */
+    public function getCacheKey()
+    {
+        return sprintf("%s/%s-%s",
+            get_class($this),
+            $this->getKey(),
+            $this->updated_at->timestamp
+        );
+    }
+}
