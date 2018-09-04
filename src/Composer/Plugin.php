531165be692ccb11a285cebe89576e934748b71b
Umut Akkaya <umutakkaya1996@gmail.com>
2018-09-05 00:41:26 +0300

    test

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -3,47 +3,18 @@
 namespace Orbitali\Composer;
 
 use Composer\Composer;
-use Composer\EventDispatcher\EventSubscriberInterface;
-use Composer\Plugin\PluginEvents;
-use Composer\Plugin\PluginInterface;
 use Composer\IO\IOInterface;
 
-class Plugin implements PluginInterface, EventSubscriberInterface
+class Plugin
 {
-    /**
-     * @var \Composer\Composer
-     */
-    protected $composer;
-    /**
-     * @var \Orbitali\Composer\AutoloadGenerator
-     */
-    protected $generator;
 
-    /**
-     * Apply plugin modifications to Composer
-     *
-     * @param Composer $composer
-     * @param IOInterface $io
-     */
-    public function activate(Composer $composer, IOInterface $io)
+    public function postAutoloadDump(Composer $composer, IOInterface $io)
     {
         var_dump("start composer plugin");
-        $this->composer = $composer;
         $this->generator = new AutoloadGenerator($composer->getEventDispatcher(), $io);
-    }
+        $this->composer = $composer;
+        var_dump($this->generator,$this->composer);
 
-    /**
-     * @return array
-     */
-    public static function getSubscribedEvents()
-    {
-        return array(
-            'post-autoload-dump' => 'dumpFiles',
-        );
-    }
-
-    public function dumpFiles()
-    {
         $extraConfig = $this->composer->getPackage()->getExtra();
         if (!array_key_exists('include_files', $extraConfig) || !is_array($extraConfig['include_files'])) {
             return;
