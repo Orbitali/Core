127af08d68d21538cba28bd2bc25fd0176b11a12
Umut Akkaya <umutakkaya1996@gmail.com>
2021-01-16 19:26:06 +0300

    fix seed

--- src/Providers/SeedServiceProvider.php
+++ src/Providers/SeedServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Orbitali\Providers;
 
-
 use Illuminate\Console\Events\CommandFinished;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Event;
@@ -12,7 +11,7 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 
 class SeedServiceProvider extends ServiceProvider
 {
-    protected $seedsPath = '/../Database/Seeds';
+    protected $seedsPath = "/../Database/Seeds";
 
     /**
      * Bootstrap services.
@@ -22,7 +21,12 @@ class SeedServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            if ($this->isConsoleCommandContains(['db:seed', '--seed'], ['--class', 'help', '-h'])) {
+            if (
+                $this->isConsoleCommandContains(
+                    ["db:seed", "--seed"],
+                    ["--class", "help", "-h"]
+                )
+            ) {
                 $this->addSeedsAfterConsoleCommandFinished();
             }
         }
@@ -37,16 +41,34 @@ class SeedServiceProvider extends ServiceProvider
      *
      * @return bool
      */
-    protected function isConsoleCommandContains($containOptions, $excludeOptions = null): bool
-    {
-        $args = Request::server('argv', null);
+    protected function isConsoleCommandContains(
+        $containOptions,
+        $excludeOptions = null
+    ): bool {
+        $args = Request::server("argv", null);
         if (is_array($args)) {
-            $command = implode(' ', $args);
-            if (str_contains($command, $containOptions) && ($excludeOptions == null || !str_contains($command, $excludeOptions))) {
+            $command = implode(" ", $args);
+            if (
+                $this->str_contains($command, $containOptions) &&
+                ($excludeOptions == null ||
+                    !$this->str_contains($command, $excludeOptions))
+            ) {
                 return true;
             }
         }
         return false;
+    }
+
+    private function str_contains($str, $arr)
+    {
+        $res = false;
+        foreach ($arr as $item) {
+            $res |= str_contains($str, $item);
+            if ($res) {
+                break;
+            }
+        }
+        return $res;
     }
 
     /**
@@ -54,7 +76,9 @@ class SeedServiceProvider extends ServiceProvider
      */
     protected function addSeedsAfterConsoleCommandFinished()
     {
-        Event::listen(CommandFinished::class, function (CommandFinished $event) {
+        Event::listen(CommandFinished::class, function (
+            CommandFinished $event
+        ) {
             // Accept command in console only,
             // exclude all commands from Artisan::call() method.
             if ($event->output instanceof ConsoleOutput) {
@@ -71,11 +95,14 @@ class SeedServiceProvider extends ServiceProvider
      */
     protected function addSeedsFrom($seedsPath)
     {
-        $file_names = glob($seedsPath . '/*.php');
+        $file_names = glob($seedsPath . "/*.php");
         foreach ($file_names as $filename) {
             $classes = $this->getClassesFromFile($filename);
             foreach ($classes as $class) {
-                Artisan::call('db:seed', ['--class' => $class, '--force' => '']);
+                Artisan::call("db:seed", [
+                    "--class" => $class,
+                    "--force" => "",
+                ]);
             }
         }
     }
@@ -91,22 +118,29 @@ class SeedServiceProvider extends ServiceProvider
         // Get namespace of class (if vary)
         $namespace = "";
         $lines = file($filename);
-        $namespaceLines = preg_grep('/^namespace /', $lines);
+        $namespaceLines = preg_grep("/^namespace /", $lines);
         if (is_array($namespaceLines)) {
             $namespaceLine = array_shift($namespaceLines);
-            $match = array();
+            $match = [];
             preg_match('/^namespace (.*);$/', $namespaceLine, $match);
             $namespace = array_pop($match);
         }
         // Get name of all class has in the file.
-        $classes = array();
+        $classes = [];
         $php_code = file_get_contents($filename);
         $tokens = token_get_all($php_code);
         $count = count($tokens);
         for ($i = 2; $i < $count; $i++) {
-            if ($tokens[$i - 2][0] == T_CLASS && $tokens[$i - 1][0] == T_WHITESPACE && $tokens[$i][0] == T_STRING) {
+            if (
+                $tokens[$i - 2][0] == T_CLASS &&
+                $tokens[$i - 1][0] == T_WHITESPACE &&
+                $tokens[$i][0] == T_STRING
+            ) {
                 $className = $tokens[$i][1];
-                $classes[] = $namespace !== "" ? $namespace . "\\$className" : $className;
+                $classes[] =
+                    $namespace !== ""
+                        ? $namespace . "\\$className"
+                        : $className;
             }
         }
         return $classes;
