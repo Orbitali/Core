48d8ff0250361fbe579fa741baef57aa36b317d0
Umut Akkaya <umutakkaya1996@gmail.com>
2021-02-07 02:01:04 +0300

    favicon added rename scripts & styles

--- src/Views/inc/app.blade.php
+++ src/Views/inc/app.blade.php
@@ -4,14 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Orbitali</title>
-
-    <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
-
-    <link rel="stylesheet" id="css-main"
-          href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
-    <link rel="stylesheet" id="css-theme" href="{{ asset('vendor/orbitali/css/dashmix.css') }}">
+    <link rel="shortcut icon" href="{{ asset('vendor/orbitali/images/favicon.png') }}">
+    <!-- <link rel="stylesheet" id="css-main" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700"> -->
+    <link rel="stylesheet" id="css-theme" href="{{ asset('vendor/orbitali/css/orbitali.css') }}">
     @stack('styles')
 </head>
 <body>
@@ -26,7 +21,7 @@
         </main>
     </div>
 @show
-<script src="{{ asset('vendor/orbitali/js/dashmix.app.js') }}"></script>
+<script src="{{ asset('vendor/orbitali/js/orbitali.core.js') }}"></script>
 @stack('scripts')
 </body>
 </html>
