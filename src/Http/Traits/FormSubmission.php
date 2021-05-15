f6bcf9d738e34f3ba74719491928536578b87179
Umut Akkaya <umutakkaya1996@gmail.com>
2021-05-16 02:21:39 +0300

    fomr submission validated by structure

--- src/Http/Traits/FormSubmission.php
+++ src/Http/Traits/FormSubmission.php
@@ -4,21 +4,26 @@ namespace Orbitali\Http\Traits;
 
 use Orbitali\Http\Models\Form;
 use Illuminate\Http\Request;
+use Orbitali\Foundations\Helpers\Structure;
 
 trait FormSubmission
 {
     public function formSubmission(Request $request)
     {
-        /** @var Form $form */
         $form = Form::where("key", $request->get("form_key"))->first();
+
+        html()->model($form);
+        list($rules, $names) = Structure::parseStructureValidations(
+            $form->structure,
+            null
+        );
+
+        $inputs = $this->validate($request, $rules, [], $names);
         $form->entries()->create([
             "ip" => $request->ip(),
-            "data" => $request->except([
-                "form_key",
-                "_token",
-                "g-recaptcha-response",
-            ]),
+            "data" => $inputs,
         ]);
+
         return redirect()->back();
     }
 }
