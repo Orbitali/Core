2a2ec64c7306fd61e679ea4e7bb2df35dcb25114
Umut Akkaya <umutakkaya1996@gmail.com>
2021-05-16 20:53:26 +0300

    structure finder updated for Form & FormEntry

--- src/Http/Traits/Structure.php
+++ src/Http/Traits/Structure.php
@@ -7,6 +7,8 @@ use Orbitali\Http\Models\Structure as StructureModel;
 use Orbitali\Http\Models\Page;
 use Orbitali\Http\Models\User;
 use Orbitali\Http\Models\Category;
+use Orbitali\Http\Models\Form;
+use Orbitali\Http\Models\FormEntry;
 
 trait Structure
 {
@@ -24,8 +26,21 @@ trait Structure
         $relationName = Relation::relationFinder($this);
         $this->cachedStructure = $this->structure()->where("mode", "self");
         if (is_a($this, Page::class) || is_a($this, Category::class)) {
+            if (isset($this->node)) {
+                $this->cachedStructure = $this->cachedStructure->union(
+                    $this->node->structure()->where("mode", $relationName)
+                );
+            }
+        } elseif (is_a($this, Form::class)) {
             $this->cachedStructure = $this->cachedStructure->union(
-                $this->node->structure()->where("mode", $relationName)
+                $this->structure()->where(
+                    "mode",
+                    Relation::relationFinder(FormEntry::class)
+                )
+            );
+        } elseif (is_a($this, FormEntry::class)) {
+            $this->cachedStructure = $this->cachedStructure->union(
+                $this->form->structure()->where("mode", $relationName)
             );
         } elseif (is_a($this, User::class)) {
             $this->cachedStructure = $this->cachedStructure->union(
