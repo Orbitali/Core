2f81a73ce303b617919e9d36f4e8e8dbc3308db7
Umut Akkaya <umutakkaya1996@gmail.com>
2021-05-30 01:39:53 +0300

    clockwork merge with own route

--- src/Routes/panel.php
+++ src/Routes/panel.php
@@ -37,3 +37,11 @@ Route::post("/file", [
     "as" => "file.upload",
     "middleware" => ["can:panel.file.upload"],
 ]);
+
+
+Route::get("clockwork", '\Clockwork\Support\Laravel\ClockworkController@webIndex');
+Route::get("clockwork/{path}", '\Clockwork\Support\Laravel\ClockworkController@webAsset')->where('path', '.+');
+Route::get('__clockwork/{id}/extended', '\Clockwork\Support\Laravel\ClockworkController@getExtendedData')->where('id', '([0-9-]+|latest)');
+Route::get('__clockwork/{id}/{direction?}/{count?}', '\Clockwork\Support\Laravel\ClockworkController@getData')->where('id', '([0-9-]+|latest)')->where('direction', '(next|previous)')->where('count', '\d+');
+Route::put('__clockwork/{id}', '\Clockwork\Support\Laravel\ClockworkController@updateData');
+Route::post('__clockwork/auth', '\Clockwork\Support\Laravel\ClockworkController@authenticate');
