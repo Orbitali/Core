268074e8acf6854181b455c94699d578fc7a013f
Umut Akkaya <umutakkaya1996@gmail.com>
2021-06-06 02:55:37 +0300

    cache added to data source

--- src/Foundations/Datasources/Languages.php
+++ src/Foundations/Datasources/Languages.php
@@ -1,14 +1,30 @@
 <?php
 namespace Orbitali\Foundations\Datasources;
 
+use Illuminate\Contracts\Cache\Repository as Cache;
+
 class Languages
 {
+    public $cache;
+
+    public function __construct(Cache $cache)
+    {
+        $this->cache = $cache;
+    }
+
     public function source()
     {
-        $languages = collect(require __DIR__ . "/../../Database/languages.php");
-        $languages = $languages->mapWithKeys(function ($q) {
-            return [$q => trans("native.language.{$q}")];
-        });
-        return $languages;
+        return $this->cache->rememberForever(
+            "orbitali.datasource.languages",
+            function () {
+                $languages = collect(
+                    require __DIR__ . "/../../Database/languages.php"
+                );
+                $languages = $languages->mapWithKeys(function ($q) {
+                    return [$q => trans("native.language.{$q}")];
+                });
+                return $languages;
+            }
+        );
     }
 }
