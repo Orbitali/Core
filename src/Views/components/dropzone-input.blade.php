06fc05c542f51460b2fc34d1dea6ba7731e78394
Umut Akkaya <umutakkaya1996@gmail.com>
2021-06-06 12:15:35 +0300

    error messages added

--- src/Views/components/dropzone-input.blade.php
+++ src/Views/components/dropzone-input.blade.php
@@ -5,8 +5,11 @@
         <span class="text-danger">*</span>
         @endif
     </label>
-    <div id="{{$id}}" class="js-dropzone w-100 form-control-file" data-name="{{$name}}"
-        data-url="{{route("panel.file.upload")}}" {{$multiple?"data-multiple":""}} data-max-files="{{$maxFiles}}"
-        data-files="{{$getFiles($model,$dottedName)}}">
+    <div id="{{$id}}" class="js-dropzone w-100 form-control-file @error($dottedName) is-invalid @enderror"
+        data-name="{{$name}}" data-url="{{route("panel.file.upload")}}" {{$multiple?"data-multiple":""}}
+        data-max-files="{{$maxFiles}}" data-files="{{$getFiles($model,$dottedName)}}">
     </div>
+    @error($dottedName)
+    <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
 </div>
--- src/Views/components/select2-input.blade.php
+++ src/Views/components/select2-input.blade.php
@@ -8,8 +8,8 @@
         <span class="text-danger">*</span>
         @endif
     </label>
-    <select id="{{$id}}" class="form-control js-select2" width="100%" name="{{$name}}"
-        {{$preventSort ? "data-prevent-sort": ""}} {{$multiple ? "multiple": ""}}>
+    <select id="{{$id}}" class="form-control js-select2 @error($dottedName) is-invalid @enderror" width="100%"
+        name="{{$name}}" {{$preventSort ? "data-prevent-sort": ""}} {{$multiple ? "multiple": ""}}>
         @php($values = $getValue($model,$dottedName))
         @foreach ($getDatasource() as $key=>$value)
         <option value="{{$key}}" {{in_array($key,$values) ? "selected": ""}}>{{$value}}</option>
