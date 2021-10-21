7b62d7a2c8793b1c053de8de62703e8c607d458d
Umut Akkaya <umutakkaya1996@gmail.com>
2021-10-21 08:45:13 +0300

    new datasources + prod pack

---
+++ src/Foundations/Datasources/InternalUrls.php
@@ -0,0 +1,26 @@
+<?php
+namespace Orbitali\Foundations\Datasources;
+
+use Orbitali\Http\Models\Url;
+use Orbitali\Foundations\Helpers\Relation;
+
+class InternalUrls
+{
+    public function source()
+    {
+        return Url::whereHas("model", function ($q) {
+            $q->whereHas("parent");
+        })
+            ->with("model.parent")
+            ->get()
+            ->mapWithKeys(function ($url) {
+                $key =
+                    Relation::relationFinder($url->model->parent) .
+                    "," .
+                    $url->model->parent->id;
+                return [
+                    $key => $url->model->name,
+                ];
+            });
+    }
+}
